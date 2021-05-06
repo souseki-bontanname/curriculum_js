4fc7115245f3bd7b9d6f5d345b191cf33f0bf2a3
souseki-bontanname <49556803+souseki-bontanname@users.noreply.github.com>
2021-05-06 20:18:56 +0900

    課題提出1−3

---
+++ JavaScript/1-3/kadai.js
@@ -0,0 +1,23 @@
+let a = 11;
+if (a != 10){
+    console.log(a);
+}
+
+let b = 16;
+if(b >=10 && b <=20 && b % 2===0){
+    console.log(b);
+}
+
+let x =10;
+if(x >= 10 && x <=20){
+    console.log('成功です');
+} else{
+    console.log('失敗です');
+}
+
+let y = 0;
+if(y % 2 === 0 ){
+    console.log('偶数です')
+}else{
+    console.log('奇数です')
+}
---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,63 @@
+let num1 = 10;
+let num2 = 4;
+result = num1 + num2;
+console.log(result);
+
+result = num1 - num2;
+console.log(result);
+
+result = num1 * num2;
+console.log(result);
+
+result = num1 / num2;
+console.log(result);
+
+result = num1 % num2;
+console.log(result);
+
+console.log('Hello' + 'World');
+let lastName = '田中';
+let firstName = '一郎';
+let fullName = lastName + firstName;
+
+console.log(fullName);
+
+let x =10;
+x++;
+console.log(x);
+let y =10;
+y--;
+console.log(y);
+
+console.log(10 === 10);
+console.log(10 === 5);
+console.log(10 !== 5);
+console.log(10 === 10);
+console.log(10 > 20);
+console.log(10 <= 10);
+
+console.log('10' === 10);
+console.log('10' == 10);
+
+let score = 0;
+if(score >= 70){
+    console.log('合格');
+} else if(score === 0){
+    console.log('0点はまずいです・・・');
+} else {
+    console.log('不合格');
+}
+
+let a = 15;
+if(a >=10 && a <= 20){
+    console.log(a);
+}
+
+let b =10;
+let c =15;
+if(b % 2 ===0){
+    console.log(b);
+}
+if(c % 2 !== 0){
+    console.log(c);
+}
