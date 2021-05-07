619faec616a0375e705cc07abea6e787c898e138
souseki-bontanname <49556803+souseki-bontanname@users.noreply.github.com>
2021-05-07 21:12:33 +0900

    課題提出1−5

---
+++ JavaScript/1−５/main.js
@@ -0,0 +1,10 @@
+function createjuice(fruits){
+    console.log(fruits + 'を受け取りました。ジュースにして返します。');
+    console.log(fruits + 'ジュースが届きました')
+}
+createjuice('みかん');
+createjuice('りんご');
+createjuice('ぶどう');
+
+
+
---
+++ JavaScript/1−５/rnndhuu.js
@@ -0,0 +1,22 @@
+function japanese(){
+    console.log('おはよう');
+    console.log('こんにちは');
+    console.log('こんばんは');
+    console.log('日本語は時間帯によって挨拶が変わります');
+    console.log('ひらがな');
+    console.log('カタカナ');
+    console.log('漢字');
+    console.log('３種類も文字があります');
+}
+japanese();
+
+function ジュース工場(果物){
+    ジュースを作る処理
+    注文した所へ発送する処理
+}
+let orangejuice = ジュース工場('みかん');
+let applejuice = ジュース工場('りんご');
+let grapejuice = ジュース工場('ぶどう');
+
+    return ジュース;
+
