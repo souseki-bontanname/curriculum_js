c410f5b0a761d48d775fa90a6a517f183439879d
souseki-bontanname <49556803+souseki-bontanname@users.noreply.github.com>
2021-05-16 09:22:13 +0900

    課題提出2−1

---
+++ JavaScript/2-1/課題/main.js
@@ -0,0 +1,7 @@
+function changeBoxColor(box,red){
+    document.getElementById('box').style.backgroundColor = 'red';
+}
+
+function rechangeBoxColor(box,blue){
+    document.getElementById('box').style.backgroundColor = 'blue';
+}
