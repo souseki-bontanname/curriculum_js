1518844b09fde195a550033465f2c04b71a42ae9
souseki-bontanname <49556803+souseki-bontanname@users.noreply.github.com>
2021-05-16 10:49:33 +0900

    課題提出2−３

---
+++ JavaScript/２/2−３/練習.js
@@ -0,0 +1,10 @@
+let box = document.getElementById('box');
+let btn = document.getElementById('btn');
+
+btn.addEventListener ('click', function(){
+    box.style.backgroundColor = 'red';
+},false);
+
+btn.addEventListener ('click', function(){
+    alert('ボタンがクリックされました')
+},false);
